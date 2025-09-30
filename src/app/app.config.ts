fcc7dad64523e62b8bc5493ee7761a0edc0c3e14
Rodrigo Perote <rodripema@hotmail.com>
2025-09-29 20:54:53 -0400

    feat: add custom PrimeNG theme and translations
    - Introduced `AppOsTheme` for custom theme configuration, replacing `Aura`.
    - Added `AppOsTranslation` for localized content.
    - Updated `app.config.ts` to use new theme and translations.

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -6,13 +6,13 @@ import {
 import { provideRouter } from '@angular/router';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { providePrimeNG } from 'primeng/config';
-import Aura from '@primeuix/themes/aura';
 import { routes } from './app.routes';
 import {IS_MOBILE, IsMobile} from './shared/services/is-mobile';
 import {CURRENT_USER, CurrentUser} from './shared/services/current-user';
 import {AppTheme, IS_DARK_MODE} from './shared/services/app-theme';
 import {provideHttpClient, withFetch, withInterceptors} from '@angular/common/http';
 import {authInterceptorFn} from './core/auth/auth-interceptor';
+import {AppOsTheme, AppOsTranslation} from '../primeng.theme';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -24,11 +24,12 @@ export const appConfig: ApplicationConfig = {
     provideAnimationsAsync(),
     providePrimeNG({
       theme: {
-        preset: Aura,
+        preset: AppOsTheme,
         options: {
           darkModeSelector: '.dark-mode'
         }
-      }
+      },
+      translation: AppOsTranslation
     }),
     {
       provide: IS_MOBILE,
