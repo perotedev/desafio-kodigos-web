6c0b2170461a1002736cccd6eb9bbc1cc777736a
Rodrigo Perote <rodripema@hotmail.com>
2025-09-30 13:48:56 -0400

    feat: update `MenuDesktop` to dynamically highlight active route
    - Added `currentRoute` tracking using Angular's `Router` and `NavigationEnd` event.
    - Modified menu item binding to compare `currentRoute` with item routes for active state logic.
    - Refactored initialization logic by implementing `OnInit` lifecycle hook.

--- src/app/core/menu/menu-desktop/menu-desktop.ts
+++ src/app/core/menu/menu-desktop/menu-desktop.ts
@@ -1,10 +1,11 @@
-import {Component, computed, inject, model, ModelSignal, Signal} from '@angular/core';
+import {Component, computed, inject, model, ModelSignal, OnInit, Signal} from '@angular/core';
 import {IS_MOBILE} from '../../../shared/services/is-mobile';
 import pkg from '../../../../../package.json';
 import {environment} from '../../../../environments/environment';
 import {IMenuItem, MenuItem} from '../menu-item/menu-item';
 import {CURRENT_USER} from '../../../shared/services/current-user';
 import {Button} from 'primeng/button';
+import {NavigationEnd, Router} from '@angular/router';
 
 const version = pkg.version;
 
@@ -17,20 +18,22 @@ const version = pkg.version;
   templateUrl: './menu-desktop.html',
   styleUrl: './menu-desktop.scss'
 })
-export class MenuDesktop {
+export class MenuDesktop implements OnInit{
   public expanded: ModelSignal<boolean> = model(false);
 
+  private readonly _router = inject(Router);
   private readonly _currentUser = inject(CURRENT_USER);
   public readonly isMobile = inject(IS_MOBILE);
 
   public readonly appVersion: string = version;
   public readonly appName: string = environment.appName;
+  public currentRoute: string = '';
 
   private readonly _menuItems: IMenuItem[] = [
     {
       label: 'Início',
       icon: 'pi pi-home',
-      route: '/',
+      route: '/home',
       roles: []
     },
     {
@@ -51,6 +54,23 @@ export class MenuDesktop {
     return this._menuItems
       .filter((item: IMenuItem) => item.roles.length === 0 || item.roles.includes(this._currentUser().role));
   });
+
+  constructor() {
+    this._router.events.subscribe((res) => {
+      if (res instanceof NavigationEnd) {
+        this.setCurrentRoute();
+      }
+    });
+  }
+
+  private setCurrentRoute(): void {
+    this.currentRoute = `/${this._router.url.split('/')[0]}`;
+    if (this.currentRoute === '/') this.currentRoute = `/${this._router.url.split('/')[1]}`;
+  }
+
+  public ngOnInit(): void {
+    this.setCurrentRoute()
+  }
 
   public toggleMenu(): void {
     if (!this.isMobile()) {
