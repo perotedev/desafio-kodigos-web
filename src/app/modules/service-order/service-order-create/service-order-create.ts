42f9dbbad8288bac42c71a0a4da5f5b21afd86b2
Rodrigo Perote <rodripema@hotmail.com>
2025-10-01 12:45:38 -0400

    feat: enhance `ServiceOrderCreate` UI for responsiveness and navigation
    - Made stepper `linear` for improved user flow.
    - Adjusted button styles to be responsive with `isMobile`.
    - Added responsive full-width button support for mobile views.
    - Incorporated navigation to the service order view on submit.
    - Refactored imports to include `RouterLink` and `IS_MOBILE`.

--- src/app/modules/service-order/service-order-create/service-order-create.ts
+++ src/app/modules/service-order/service-order-create/service-order-create.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component, inject} from '@angular/core';
 import {ButtonModule} from 'primeng/button';
 import {StepperModule} from 'primeng/stepper';
 import {ServiceOrderForm} from '../service-order-form/service-order-form';
@@ -6,14 +6,17 @@ import {ServiceOrderItems} from '../service-order-items/service-order-items';
 import {ServiceOrderImages} from '../service-order-images/service-order-images';
 import {IServiceOrder} from '../../../shared/interfaces/IServiceOrder';
 import {IServiceType} from '../../../shared/interfaces/IServiceType';
+import {RouterLink} from '@angular/router';
+import {IS_MOBILE} from '../../../shared/services/is-mobile';
 
 @Component({
   selector: 'app-service-order-create',
-  imports: [StepperModule, ButtonModule, ServiceOrderForm, ServiceOrderItems, ServiceOrderImages],
+  imports: [StepperModule, ButtonModule, ServiceOrderForm, ServiceOrderItems, ServiceOrderImages, RouterLink],
   templateUrl: './service-order-create.html',
   styleUrl: './service-order-create.scss'
 })
 export class ServiceOrderCreate {
+  public readonly isMobile = inject(IS_MOBILE);
   public createdSo?: IServiceOrder;
   public serviceTypes: IServiceType[] = [];
 }
